3654e826adaff025b12c43a2f55add74fba8ab54
Martin <martinez.forestier@gmail.com>
2021-06-24 15:29:54 +0200

    V2 project
    ajout formulaire

--- V2/app/views.py
+++ V2/app/views.py
@@ -2,7 +2,20 @@ from flask import render_template, request, abort, redirect, url_for, Flask
 from app import app
 import datetime
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     date = datetime.datetime.now().strftime("%x %X")
     return render_template('index.html', date=date)
+
+
+@app.route('/form_predict', methods=['GET', 'POST'])
+def form_predict():
+    date = datetime.datetime.now().strftime("%x %X")
+    return render_template('form_predict.html', date=date)
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    date = datetime.datetime.now().strftime("%x %X")
+    return render_template('predict.html', date=date)
