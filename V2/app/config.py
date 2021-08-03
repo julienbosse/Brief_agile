7ca900f107f1193920d8e7f32501629347345fd7
Martin <martinez.forestier@gmail.com>
2021-08-03 11:07:55 +0200

    fix MP

--- V2/app/config.py
+++ V2/app/config.py
@@ -1,2 +1,2 @@
 SECRET_KEY = 'p9Bv<3Eid9%$i01'
-SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://martin:simplon!21M@localhost/test'
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://martin:**********/test'
